fe5fc8976022c52a4f89b7e6982ed7e5b9e8dc2a
Christoffer Ekblom <cekmw09@student.lnu.se>
2018-04-19 00:50:18 +0200

    ➖ constants.js deletion

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ const express = require('express')
 const exphbs  = require('express-handlebars')
 const app = express()
 const port = process.env.PORT
-const constants = require('./lib/constants')
 const requireDir = require('require-dir');
 const routes = requireDir('./routes');
 
-app.use(constants.PUBLIC_DIR, express.static('./' + constants.PUBLIC_DIR))
+app.use("/public", express.static('./public'))
 
 for(let i in routes) {
   app.use('/', routes[i])
